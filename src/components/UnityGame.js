68e3b60b0349fcb0c75ebaa58c182f5922a76e33
Yoshiko <67814417+yoshiko2000@users.noreply.github.com>
2021-07-06 22:42:20 -0400

    Updated UnityGame.js

--- src/components/UnityGame.js
+++ src/components/UnityGame.js
@@ -2,11 +2,7 @@ import React from 'react';
 
 class UnityGame extends React.Component {
     render() {
-        return <iframe 
-        frameborder="0" 
-        src="https://itch.io/embed-upload/4102294?color=333333" 
-        allowfullscreen="" width="1480" height="940">  
-        </iframe>;
+        return <iframe frameborder="0" src="https://itch.io/embed-upload/4124416?color=333333" allowfullscreen="" width="1920" height="1100"></iframe>;
     }
 }
 export default UnityGame
