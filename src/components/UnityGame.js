eefcfe13fc6efa10d5fde6f424bf708e62c44916
Yoshiko <67814417+yoshiko2000@users.noreply.github.com>
2021-07-06 22:30:54 -0400

    Created Unity Game containing the iFrame

---
+++ src/components/UnityGame.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class UnityGame extends React.Component {
+    render() {
+        return <iframe 
+        frameborder="0" 
+        src="https://itch.io/embed-upload/4102294?color=333333" 
+        allowfullscreen="" width="1480" height="940">  
+        </iframe>;
+    }
+}
+export default UnityGame
